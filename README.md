f2c356b5cb9bf5f736f0aaba88f71c395cb275c7
Pulkit gera <pulkithifi@yahoo.com>
2018-10-19 12:18:45 +0530

    all questions added

---
+++ README.md
@@ -0,0 +1,5 @@
+# Alexa Skills
+
+This repo contains the lambda code for alexa skills created by me. Currently this is the first skill that i am creating called "Naruto Quiz"
+
+Big Shout Out to [This repo](https://github.com/alexa/skill-sample-nodejs-trivia) which helped me create and understand
---
+++ questions.js
@@ -0,0 +1,471 @@
+module.exports = {
+    /**
+     * When editing your questions pay attention to your punctuation.
+     * Make sure you use question marks.
+     * Make sure the first answer is the correct one.
+     * Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+     */
+    QUESTIONS_EN_GB: [
+      {
+        'Which year did Naruto come out?': [
+          '2002',
+          '2001',
+          '2007',
+          '2005',
+          '2003',
+          '1999',
+        ],
+      },
+      {
+        'Who is Naruto\'s Dad? ': [
+          'Minato Uzumaki',
+          'Kakashi Hatake',
+          'Jiraya',
+          'Orochimaru',
+          'Itachi Uchiha',
+          'Sasuke Uchiha',
+        ],
+      },
+      {
+        'Who is responsible for massacaring entire Uchiha Clan?': [
+          'Itachi Uchiha',
+          'Sasuke Uchiha',
+          'Madara Uchiha',
+          'Tobi Uchiha',
+        ],
+      },
+      {
+        'Who is Naruto\'s wife? ': [
+          'Hinata',
+          'Sakura',
+          'Ino',
+          'Kirin',
+        ],
+      },
+      {
+        'What is the name of the nine tail fox?': [
+          'Kurama',
+          'Sasuke',
+          'Killer Bee',
+          'Nagato',
+        ],
+      },
+      {
+        'Which of them was not a Hokage?': [
+          'Danzo',
+          'Naruto',
+          'Tobirama',
+          'Tsunade',
+          'Minato',
+        ],
+      },
+      {
+        'Who is the author of the manga?': [
+          'Masashi Kishimoto',
+          'Hideaki Ano',
+          'Hiromi Arakawa',
+          'Kohei Horikoshi',
+        ],
+      },
+      {
+        'Which of these killed Hidan?': [
+          'He died of starvation',
+          'Shikamaru',
+          'Kakashi',
+          'Naruto',
+        ],
+      },
+      {
+        'How many great nations are there?': [
+          '5',
+          '3',
+          '11',
+          '33',
+        ],
+      },
+      {
+        'At the end of \'Naruto\' where did Naruto and Sasuke fight?': [
+          'Valley of Death',
+          'Hokage Rock',
+          'Genbu',
+          'Academy',
+        ],
+      },
+      {
+        'What was Gaara\'s speciality?': [
+          'Sand',
+          'Fire',
+          'Snakes',
+          'Water',
+        ],
+      },
+      {
+        'Which of these was not a Genjutsu user?': [
+          'Rock Lee',
+          'Sasuke',
+          'Itachi',
+          'Madara',
+        ],
+      },
+      {
+        'Who was the first Hokage?': [
+          'Hashirama',
+          'Tobirama',
+          'Kakashi',
+          'Tsunade',
+        ],
+      },
+      {
+        'Who was the founder of the criminal organization Akatsuki?': [
+          'Yahiko',
+          'Madara',
+          'Orochimaru',
+          'Obito',
+        ],
+      },
+      {
+        'Which of them was not part of Team 7?': [
+          'Ino',
+          'Sakura',
+          'Naruto',
+          'Sasuke',
+        ],
+      },
+      {
+        'Which of them was not part of the Hyuga Clan?': [
+          'Naruto',
+          'Hinata',
+          'Neji',
+          'Hiashi',
+        ],
+      },
+      {
+        'Which of them was not a student of Hiruzen?': [
+          'Minato',
+          'Jiraya',
+          'Tsunade',
+          'Orochimaru',
+        ],
+      },
+      {
+        'Who was Naruto\'s Mom?': [
+          'Kushina',
+          'Tsunade',
+          'Sakura',
+          'Ino',
+        ],
+      },
+      {
+        'Who was responsible for the Third Great Ninja War?': [
+          'Obito',
+          'Orochimaru',
+          'Nagato',
+          'Madara',
+        ],
+      },
+      {
+        'Who said the following - \" Those who break rules are scum. But those who abandon their friends are worse than scum.\"?': [
+          'Kakashi',
+          'Naruto',
+          'Jiraya',
+          'Hashirama',
+        ],
+      },
+      {
+        'Historically, the Uchiha Clan were in direct conflict with which clan?': [
+          'Senju',
+          'Uzumaki',
+          'Hyuga',
+          'Sarutobi',
+        ],
+      },
+      {
+        'Which clan is famous for Sharingan?': [
+          'Uchiha',
+          'Senju',
+          'Hyuga',
+          'Uzumaki',
+        ],
+      },
+      {
+        'Which clan is famous for possesing Byakugan?': [
+          'Hyuga',
+          'Uchiha',
+          'Sarutobi',
+          'Uzumaki',
+        ],
+      },
+      {
+        'Which clan is famous for possesing Rinnegan?': [
+          'Otsutsuki',
+          'Hyuga',
+          'Uchiha',
+          'Senju',
+        ],
+      },
+      {
+        'Which of them did not posses any special eye?': [
+          'Rock Lee',
+          'Sasuke',
+          'Hinata',
+          'Nagato',
+        ],
+      },
+      {
+        'Which of them was famous for his exploding clay technique/art?': [
+          'Deidara',
+          'Obito',
+          'Kissame',
+          'Hidan',
+        ],
+      },
+      {
+        'Which of them was not a part of Team 7 in Shippudden?': [
+          'Sasuke',
+          'Sai',
+          'Naruto',
+          'Sakura',
+        ],
+      },
+      {
+        'Which of them becomes a consultant to Naruto at end of Shippudden?': [
+          'Shikmaru',
+          'Sasuke',
+          'Boruto',
+          'Hinata',
+        ],
+      },
+    ],
+    QUESTIONS_EN_US: [
+      {
+        'Which year did Naruto come out?': [
+          '2002',
+          '2001',
+          '2007',
+          '2005',
+          '2003',
+          '1999',
+        ],
+      },
+      {
+        'Who is Naruto\'s Dad? ': [
+          'Minato Uzumaki',
+          'Kakashi Hatake',
+          'Jiraya',
+          'Orochimaru',
+          'Itachi Uchiha',
+          'Sasuke Uchiha',
+        ],
+      },
+      {
+        'Who is responsible for massacaring entire Uchiha Clan?': [
+          'Itachi Uchiha',
+          'Sasuke Uchiha',
+          'Madara Uchiha',
+          'Tobi Uchiha',
+        ],
+      },
+      {
+        'Who is Naruto\'s wife? ': [
+          'Hinata',
+          'Sakura',
+          'Ino',
+          'Kirin',
+        ],
+      },
+      {
+        'What is the name of the nine tail fox?': [
+          'Kurama',
+          'Sasuke',
+          'Killer Bee',
+          'Nagato',
+        ],
+      },
+      {
+        'Which of them was not a Hokage?': [
+          'Danzo',
+          'Naruto',
+          'Tobirama',
+          'Tsunade',
+          'Minato',
+        ],
+      },
+      {
+        'Who is the author of the manga?': [
+          'Masashi Kishimoto',
+          'Hideaki Ano',
+          'Hiromi Arakawa',
+          'Kohei Horikoshi',
+        ],
+      },
+      {
+        'Which of these killed Hidan?': [
+          'He died of starvation',
+          'Shikamaru',
+          'Kakashi',
+          'Naruto',
+        ],
+      },
+      {
+        'How many great nations are there?': [
+          '5',
+          '3',
+          '11',
+          '33',
+        ],
+      },
+      {
+        'At the end of \'Naruto\' where did Naruto and Sasuke fight?': [
+          'Valley of Death',
+          'Hokage Rock',
+          'Genbu',
+          'Academy',
+        ],
+      },
+      {
+        'What was Gaara\'s speciality?': [
+          'Sand',
+          'Fire',
+          'Snakes',
+          'Water',
+        ],
+      },
+      {
+        'Which of these was not a Genjutsu user?': [
+          'Rock Lee',
+          'Sasuke',
+          'Itachi',
+          'Madara',
+        ],
+      },
+      {
+        'Who was the first Hokage?': [
+          'Hashirama',
+          'Tobirama',
+          'Kakashi',
+          'Tsunade',
+        ],
+      },
+      {
+        'Who was the founder of the criminal organization Akatsuki?': [
+          'Yahiko',
+          'Madara',
+          'Orochimaru',
+          'Obito',
+        ],
+      },
+      {
+        'Which of them was not part of Team 7?': [
+          'Ino',
+          'Sakura',
+          'Naruto',
+          'Sasuke',
+        ],
+      },
+      {
+        'Which of them was not part of the Hyuga Clan?': [
+          'Naruto',
+          'Hinata',
+          'Neji',
+          'Hiashi',
+        ],
+      },
+      {
+        'Which of them was not a student of Hiruzen?': [
+          'Minato',
+          'Jiraya',
+          'Tsunade',
+          'Orochimaru',
+        ],
+      },
+      {
+        'Who was Naruto\'s Mom?': [
+          'Kushina',
+          'Tsunade',
+          'Sakura',
+          'Ino',
+        ],
+      },
+      {
+        'Who was responsible for the Third Great Ninja War?': [
+          'Obito',
+          'Orochimaru',
+          'Nagato',
+          'Madara',
+        ],
+      },
+      {
+        'Who said the following - \" Those who break rules are scum. But those who abandon their friends are worse than scum.\"?': [
+          'Kakashi',
+          'Naruto',
+          'Jiraya',
+          'Hashirama',
+        ],
+      },
+      {
+        'Historically, the Uchiha Clan were in direct conflict with which clan?': [
+          'Senju',
+          'Uzumaki',
+          'Hyuga',
+          'Sarutobi',
+        ],
+      },
+      {
+        'Which clan is famous for Sharingan?': [
+          'Uchiha',
+          'Senju',
+          'Hyuga',
+          'Uzumaki',
+        ],
+      },
+      {
+        'Which clan is famous for possesing Byakugan?': [
+          'Hyuga',
+          'Uchiha',
+          'Sarutobi',
+          'Uzumaki',
+        ],
+      },
+      {
+        'Which clan is famous for possesing Rinnegan?': [
+          'Otsutsuki',
+          'Hyuga',
+          'Uchiha',
+          'Senju',
+        ],
+      },
+      {
+        'Which of them did not posses any special eye?': [
+          'Rock Lee',
+          'Sasuke',
+          'Hinata',
+          'Nagato',
+        ],
+      },
+      {
+        'Which of them was famous for his exploding clay technique/art?': [
+          'Deidara',
+          'Obito',
+          'Kissame',
+          'Hidan',
+        ],
+      },
+      {
+        'Which of them was not a part of Team 7 in Shippudden?': [
+          'Sasuke',
+          'Sai',
+          'Naruto',
+          'Sakura',
+        ],
+      },
+      {
+        'Which of them becomes a consultant to Naruto at end of Shippudden?': [
+          'Shikmaru',
+          'Sasuke',
+          'Boruto',
+          'Hinata',
+        ],
+      },
+    ],
+  };
+  
